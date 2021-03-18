7aaf924a19a42c3a8aa6d609947bab710d9424d4
RishalAggarwal <rishal.aggarwal@gmail.com>
2021-03-18 13:41:58 +0530

    Create clean_pdb.py

---
+++ clean_pdb.py
@@ -0,0 +1,17 @@
+from Bio.PDB import PDBParser, PDBIO, Select
+import Bio
+import os
+import sys
+
+class NonHetSelect(Select):
+    def accept_residue(self, residue):
+        return 1 if Bio.PDB.Polypeptide.is_aa(residue,standard=True) else 0
+
+def clean_pdb(input_file,output_file):
+    pdb = PDBParser().get_structure("protein", input_file)
+    io = PDBIO()
+    io.set_structure(pdb)
+    io.save(output_file, NonHetSelect())
+    
+if __name__ == '__main__':
+    clean_pdb((sys.argv[1],sys.argv[2])
